1e28ea482502495102e6317b1449aeeea57438e6
Thomas H Jones II <github@xanthia.com>
2017-04-18 15:05:30 -0400

    Add support for EFS-based persistence of git repository data

--- SupportFiles/gitlab_instprep.sh
+++ SupportFiles/gitlab_instprep.sh
@@ -2,7 +2,6 @@
 #
 # Script to handle preparation of the instance for installing
 # and configuring GitLab
-#
 #################################################################
 PROGNAME=$(basename "${0}")
 LOGFACIL="user.err"
@@ -15,17 +14,20 @@ then
   OSDIST="os=el&dist=7"
 fi
 REPOSRC=${REPOSRC:-https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/config_file.repo?${OSDIST}}
+GITLAB_EFS_HOST=${GITLAB_EFS_HOST:-UNDEF}
 INSTRPMS=()
 DEPRPMS=(
           curl
           policycoreutils
           openssh-server
           openssh-clients
+          nfs-utils
         )
 FWPORTS=(
           80
           443
         )
+
 
 # Log failures and exit
 function error_exit {
@@ -34,12 +36,12 @@ function error_exit {
    exit 1
 }
 
+
 # Open firewall ports
 function FwStuff {
    # Temp-disable SELinux (need when used in cloud-init context)
-   setenforce 0 || \
+   setenforce 0 && echo "Temp-disabled SELinux" || \
       error_exit "Failed to temp-disable SELinux"
-   echo "Temp-disabled SELinux"
 
    if [[ $(systemctl --quiet is-active firewalld)$? -eq 0 ]]
    then
@@ -49,10 +51,10 @@ function FwStuff {
       ${FWCMD} --enabled
    fi
 
-   for PORT in "${FWPORTS[@]}"
+   for PORT in ${FWPORTS[@]}
    do
-      printf "Add firewall exception for port %s... " "${PORT}"
-      ${FWCMD} --permanent "--add-port=${PORT}/tcp" || \
+      printf "Add firewall exception for port ${PORT}... "
+      ${FWCMD} --permanent --add-port=${PORT}/tcp || \
          error_exit "Failed to add port ${PORT} to firewalld"
    done
 
@@ -62,16 +64,40 @@ function FwStuff {
       error_exit "Failed to reload firewalld rules"
 
    # Restart SELinux
-   setenforce 1 || \
+   setenforce 1 && echo "Re-enabled SELinux" || \
       error_exit "Failed to reactivate SELinux"
-   echo "Re-enabled SELinux"
+
 }
 
+
+# Handle any requested EFS-mounts
+function EfsMount {
+   local MNTOPT="rw,relatime,vers=4.0,rsize=1048576,wsize=1048576,hard,proto=tcp,timeo=600,retrans=2,sec=sys,local_lock=none"
+   local EFSDIR="${1}"
+   local MNTDIR="${2}"
+
+   printf "%s:%s\t%s\tnfs4\t%s\t0 0\n" ${GITLAB_EFS_HOST} ${EFSDIR} ${MNTDIR} ${MNTOPT} >> /etc/fstab
+
+   # Create mount-dir as necessary
+   if [[ ! -d ${MNTDIR} ]]
+   then
+      mkdir -p ${MNTDIR} || \
+         error_exit "Failed to create ${MNTDIR}"
+   fi
+
+   mount ${MNTDIR} || error_exit "Failed to mount EFS-resource to ${MNTDIR}"
+}
+
+
+###
+# Main
+###
+
 # Check if we're missing any vendor-enumerated RPMs
-for RPM in "${DEPRPMS[@]}"
+for RPM in ${DEPRPMS[@]}
 do
-   printf "Cheking for presence of %s... " "${RPM}"
-   if [[ $(rpm --quiet -q "$RPM")$? -eq 0 ]]
+   printf "Cheking for presence of ${RPM}... "
+   if [[ $(rpm --quiet -q $RPM)$? -eq 0 ]]
    then
       echo "Already installed."
    else
@@ -83,8 +109,8 @@ done
 # Install any missing vendor-enumerated RPMs
 if [[ ${#INSTRPMS[@]} -ne 0 ]]
 then
-   echo "Will attempt to install the following RPMS: ${INSTRPMS[*]}"
-   yum install -y "${INSTRPMS[@]}" || \
+   echo "Will attempt to install the following RPMS: ${INSTRPMS[@]}"
+   yum install -y ${INSTRPMS[@]} || \
       error_exit "Install of RPM-dependencies experienced failures"
 else
    echo "No RPM-dependencies to satisfy"
@@ -95,17 +121,17 @@ for MGSVC in sshd postfix
 do
    if [[ $(systemctl --quiet is-active ${MGSVC})$? -ne 0 ]]
    then
-      printf "Starting %s..." ${MGSVC}
-      systemctl start ${MGSVC} || \
+      printf "Starting ${MGSVC}... "
+      systemctl start ${MGSVC} && \
+         echo "Success!" || \
          error_exit "Failed to start ${MGSVC}!"
-      echo "Success!"
    fi
    if [[ $(systemctl --quiet is-enabled ${MGSVC})$? -ne 0 ]]
    then
-      printf "Enabling %s..." ${MGSVC}
-      systemctl enable ${MGSVC} || \
+      printf "Enabling ${MGSVC}... "
+      systemctl enable ${MGSVC} && \
+         echo "Success!" || \
          error_exit "Failed to enable ${MGSVC}!"
-      echo "Success!"
    fi
 done
 
@@ -113,22 +139,16 @@ done
 FwStuff
 
 # Install repo-def for repository hosting the GitLab RPM(s)
-curl -skL "${REPOSRC}" -o /etc/yum.repos.d/GitLab.repo || \
-   error_exit "Failed to install repodef for GitLab CE"
-echo "Successfully installed repodef for GitLab CE"
+curl -skL "${REPOSRC}" -o /etc/yum.repos.d/GitLab.repo
 # Ensure SCL repositories are available
 RELEASE=$(rpm -qf /etc/redhat-release --qf '%{name}')
 if [[ $(yum repolist all | grep -q scl)$? -ne 0 ]]
 then
-   yum install -y "${RELEASE}-scl" || \
-      error_exit "Attempted install of Software CoLlections repodefs failed."
-   echo "Successfully nstalled Software CoLlections repodefs"
+   yum install -y ${RELEASE}-scl
 fi
 
 # Install a Ruby version that is FIPS compatible
-yum --enablerepo=*scl* install -y rh-ruby23 || \
-   error_exit "Install of updated Ruby RPM failed."
-echo "Installed updated Ruby RPM"
+yum --enablerepo=*scl* install -y rh-ruby23
 
 # Permanently eable the SCL version of Ruby
 cat << EOF > /etc/profile.d/scl-ruby.sh
@@ -136,23 +156,18 @@ source /opt/rh/rh-ruby23/enable
 export X_SCLS="\$(scl enable rh-ruby23 'echo \$X_SCLS')"
 EOF
 
-# shellcheck disable=SC1091
-source /etc/profile.d/scl-ruby.sh || \
-   error_exit "Failed to reset Ruby-location to updated version"
-echo "Reset Ruby-location to updated version"
-
 # Disable Chef's FIPS stuff
 cat << EOF > /etc/profile.d/chef.sh
 export CHEF_FIPS=""
 EOF
 
-# shellcheck disable=SC1091
-source /etc/profile.d/chef.sh || \
-   error_exit "Failed to shut off FIPS-checking in embedded Chef Gem"
-echo "Shut off FIPS-checking in embedded Chef Gem"
 
-# Do base-install of GitLab RPM
-printf "Installing GitLab CE"
-yum install -y gitlab-ce || \
-   error_exit "Install failed."
-echo "Install succeeded. Gitlab must now be configured"
+# Install (but don't configure) GitLab
+if [[ ${GITLAB_EFS_HOST} = UNDEF ]]
+then
+   echo "No EFS host was declared in the template."
+   echo "Assuming alternate method for persisting repository data in use..."
+else
+   EfsMount "/git-data" "/var/opt/gitlab/git-data"
+fi
+yum install -y gitlab-ce
